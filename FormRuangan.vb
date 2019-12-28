618fa88a4603a166bb802f1664b8d12265a2c469
Irpan Budiana <irpan.budiana@gmail>
2019-12-28 11:11:32 +0700

    form master ruangan

--- FormRuangan.vb
+++ FormRuangan.vb
@@ -15,6 +15,8 @@ Public Class FormRuangan
 
     Sub tambah()
         DataGridView1.Enabled = False
+        TextBoxKodeRuangan.ReadOnly = False
+
         TextBoxKodeRuangan.Clear()
         TextBoxNamaRuangan.Clear()
         TextBoxKodeRuangan.Enabled = True
@@ -140,6 +142,7 @@ Public Class FormRuangan
     End Sub
 
     Private Sub DataGridView1_CellClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        TextBoxKodeRuangan.ReadOnly = True
         bukaDB()
         CMD = New MySqlCommand("select * from ruangan where koderuangan = '" & TextBoxKodeRuangan.Text & "'", Koneksi)
 
@@ -154,4 +157,23 @@ Public Class FormRuangan
     End Sub
 
   
+    Private Sub ButtonHapus_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonHapus.Click
+        If TextBoxKodeRuangan.Text = "" Then
+            MessageBox.Show("Pilih dahulu data ruangan yang akan Anda hapus pada tabel di atas!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            If TextBoxKodeRuangan.Text <> "" Then
+                If MessageBox.Show("Apakah Anda yakin akan mengahapus data ruangan ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then
+                    Call bukaDB()
+                    hapus = "delete from ruangan where koderuangan = '" & TextBoxKodeRuangan.Text & "'"
+                    CMD = New MySqlCommand(hapus, Koneksi)
+
+                    CMD.ExecuteNonQuery()
+                    Call isiGrid()
+                    Call bersih()
+                    TextBoxKodeRuangan.Focus()
+                    MessageBox.Show("Data ruangan berhasil dihapus", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            End If
+        End If
+    End Sub
 End Class
